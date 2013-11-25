19640c31c22c15ce64ef960d9a9f51fabcf4be13
Chris McKenzie <kristopolous@yahoo.com>
2013-11-24 20:15:31 -0800

    Key period test (#29)

---
+++ tests/0029.key_period.test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Bug 29: Problem when key contains period character
+#
+# See:
+# https://github.com/kristopolous/TickTick/issues/29
+#
+# And:
+# https://groups.google.com/forum/#!topic/ticktick-project/CbOZjQ0RGmc
+
+. ../ticktick.sh
+
+# This code should genuinely work.
+# The tricky part is the second key, where you see the helloService(.)
+``{
+  "bootstrap" : {
+    "healthy" : true
+  },
+  "helloService.checkGreetingHealthCheck" : {
+    "healthy" : true,
+    "message" : "Greeting set to Hello"
+  }
+}
+``
